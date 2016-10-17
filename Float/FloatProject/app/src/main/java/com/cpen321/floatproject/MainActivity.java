0ed408399bbdf6d25c93f89a4b88dee96d921011
Samuel Farinas <samuelfarinas@gmail.com>
2016-10-17 11:27:06 -0700

    Added theme color. Added fake log-in button.

--- Float/FloatProject/app/src/main/java/com/cpen321/floatproject/MainActivity.java
+++ Float/FloatProject/app/src/main/java/com/cpen321/floatproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cpen321.floatproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.startbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //start _Submenu activity
+                Intent intent = new Intent(v.getContext(), Map.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ Float/FloatProject/app/src/main/java/com/cpen321/floatproject/Map.java
@@ -0,0 +1,18 @@
+package com.cpen321.floatproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sfarinas on 10/17/2016.
+ */
+public class Map extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+
+
+    }
+}
