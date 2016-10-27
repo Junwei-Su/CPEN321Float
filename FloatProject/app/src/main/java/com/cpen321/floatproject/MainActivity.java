d4d6bf5686020895b595be7f0b480ba5ac59fff4
Samuel Farinas <samuelfarinas@gmail.com>
2016-10-27 10:48:32 -0700

    Added Facebook login button.

--- FloatProject/app/src/main/java/com/cpen321/floatproject/MainActivity.java
+++ FloatProject/app/src/main/java/com/cpen321/floatproject/MainActivity.java
@@ -1,27 +1,29 @@
 package com.cpen321.floatproject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.provider.ContactsContract;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.facebook.Profile;
+
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.startbutton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //start _Submenu activity
-                Intent intent = new Intent(v.getContext(), Map.class);
-                startActivity(intent);
-            }
-        });
+        if(Profile.getCurrentProfile()!=null)
+            startMapActivity();
 
+    }
+
+    //starts Map activity
+    private void startMapActivity(){
+        Intent intent = new Intent(this, Map.class);
+        startActivity(intent);
     }
 }
