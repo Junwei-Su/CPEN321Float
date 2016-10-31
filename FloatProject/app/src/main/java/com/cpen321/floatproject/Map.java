183d8cf4050f54d8779225c61a681d9801c1ba27
Samuel Farinas <samuelfarinas@gmail.com>
2016-10-31 00:49:23 -0700

    Have infowindow pop-up when marker is clicked and disappear when x button is clicked. Google logo still covered on map creation.

--- FloatProject/app/src/main/java/com/cpen321/floatproject/Map.java
+++ FloatProject/app/src/main/java/com/cpen321/floatproject/Map.java
@@ -1,13 +1,19 @@
 package com.cpen321.floatproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -16,17 +22,64 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by sfarinas on 10/17/2016.
  */
-public class Map extends FragmentActivity implements OnMapReadyCallback {
+public class Map extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener {
+
+    GoogleMap map;
+    int buttonpanelheight;
+    int infowindowheight;
+
+    LatLng ubc = new LatLng(49.261312, -123.253783);
+    CameraPosition defaultcamerapos = new CameraPosition.Builder()
+            .target(ubc)   // Sets the center of the map to Mountain View
+            .zoom(10)                   // Sets the zoom
+            .bearing(0)                // Sets the orientation of the camera to east
+            .tilt(0)                   // Sets the tilt of the camera to 30 degrees
+            .build();                   // Creates a CameraPosition from the builder
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
+
+        LinearLayout infowindow = (LinearLayout) findViewById(R.id.infowindow);
+        infowindow.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        //called when layout is ready yet before drawn
+
+                        LinearLayout infowindow = (LinearLayout) findViewById(R.id.infowindow);
+
+                        infowindowheight = infowindow.getHeight();
+                    }
+                }
+        );
+
+        LinearLayout mapbuttonpanel = (LinearLayout) findViewById(R.id.mapbuttonpanel);
+        infowindow.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        //called when layout is ready yet before drawn
+
+                        LinearLayout mapbuttonpanel = (LinearLayout) findViewById(R.id.mapbuttonpanel);
+
+                        buttonpanelheight = mapbuttonpanel.getHeight();
+                    }
+                }
+        );
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -50,15 +103,29 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
                 startActivity(intent);
             }
         });
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.closeWindow);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout infowindow = (LinearLayout) findViewById(R.id.spacerparent);
+                infowindow.setVisibility(View.GONE);
+
+                map.setPadding(0, 0, 0, buttonpanelheight);
+                map.animateCamera(CameraUpdateFactory.newCameraPosition(defaultcamerapos));
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
-        LatLng ubc = new LatLng(49.261312, -123.253783);
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mapbuttonpanel);
-        int height = linearLayout.getHeight();
+        this.map = map;
 
-        map.setPadding(0, 0, 0, height);
+        //disable toolbar from appearing when a marker is clicked
+        map.getUiSettings().setMapToolbarEnabled(false);
 
         map.addMarker(new MarkerOptions()
                 .position(ubc)
@@ -70,12 +137,68 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
                 .strokeColor(ContextCompat.getColor(this, R.color.darkerbackground))
                 .fillColor(ContextCompat.getColor(this, R.color.background)));
 
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(ubc)   // Sets the center of the map to Mountain View
-                .zoom(10)                   // Sets the zoom
-                .bearing(90)                // Sets the orientation of the camera to east
-                .tilt(0)                   // Sets the tilt of the camera to 30 degrees
-                .build();                   // Creates a CameraPosition from the builder
-        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        map.setOnMarkerClickListener(this);
+
+        map.setPadding(0, 0, 0, buttonpanelheight);
+
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(defaultcamerapos));
+    }
+
+    /**
+     * Called when the user clicks a marker.
+     */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        // Can retrieve the data from the marker.
+        //Integer clickCount = (Integer) marker.getTag();
+
+        LinearLayout campinfo = (LinearLayout) findViewById(R.id.spacerparent);
+        campinfo.setVisibility(View.VISIBLE);
+
+        map.setPadding(0, 0, 0, buttonpanelheight + infowindowheight);
+
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(defaultcamerapos));
+
+        return false;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Map Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cpen321.floatproject/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Map Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.cpen321.floatproject/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
