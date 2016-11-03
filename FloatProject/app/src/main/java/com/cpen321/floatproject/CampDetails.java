aaf92501128b4dde43ce452c8d6b37f38d880841
Richard Xie <xiezihao@yahoo.ca>
2016-11-02 19:45:31 -0700

    Revert "revert"
    This reverts commit a5a00bfe92e19725c36e57fb7446e76ab4b618c3.

--- FloatProject/app/src/main/java/com/cpen321/floatproject/CampDetails.java
+++ FloatProject/app/src/main/java/com/cpen321/floatproject/CampDetails.java
@@ -6,12 +6,12 @@ import android.os.Bundle;
 /**
  * Created by sfarinas on 10/17/2016.
  */
-public class JoinCampaign extends Activity {
+public class CampDetails extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.joincampaign);
+        setContentView(R.layout.campdetails);
 
     }
 }
