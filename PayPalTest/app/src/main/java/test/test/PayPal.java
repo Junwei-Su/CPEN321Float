5ae8c96314dfd55cc1a64003c8e22a0603240feb
Rachel <yrachel788@gmail.com>
2016-11-04 15:26:45 -0700

    started implementing future payments

--- PayPalTest/app/src/main/java/test/test/PayPal.java
+++ PayPalTest/app/src/main/java/test/test/PayPal.java
@@ -2,15 +2,13 @@ package test.test;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.net.Uri;
 import com.paypal.android.sdk.payments.PayPalAuthorization;
 import com.paypal.android.sdk.payments.PayPalConfiguration;
 import com.paypal.android.sdk.payments.PayPalFuturePaymentActivity;
@@ -19,15 +17,11 @@ import com.paypal.android.sdk.payments.PayPalService;
 import com.paypal.android.sdk.payments.PaymentActivity;
 import com.paypal.android.sdk.payments.PaymentConfirmation;
 
+import org.json.JSONException;
+
 import java.math.BigDecimal;
 
 public class PayPal extends AppCompatActivity {
-
-    //holds the status (pink icon)
-    TextView p_response;
-
-    //The PayPal serivce
-    Intent p_service;
 
     //set environment as the testing sandbox (can also be set to do actual transactions)
     private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
@@ -36,32 +30,42 @@ public class PayPal extends AppCompatActivity {
     //configure PayPal with the environment and client ID
     private static PayPalConfiguration p_configuration = new PayPalConfiguration()
             .environment(CONFIG_ENVIRONMENT)
-            .clientId(CLIENT_ID);
+            .clientId(CLIENT_ID)
+            .merchantName("test")
+            .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
+            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
 
     //identifying numbers to distinguish between instant payments and future payments
     private static final int REQUEST_CODE_PAYMENT = 101;
     private static final int REQUEST_CODE_FUTURE_PAYMENT = 102;
+
+    private int future_payment_amount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_pal);
 
-        p_response = (TextView) findViewById(R.id.response);
-        p_service = new Intent(this, PayPalService.class);
+        Intent p_service = new Intent(this, PayPalService.class);
 
         p_service.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, p_configuration);
         startService(p_service); //the PayPal service
     }
 
-    void makeDonation(View view){
-        //get the user input then convert it to an integer
-        EditText editText = (EditText) findViewById(R.id.user_input);
-       String s_amount = editText.getText().toString();
+    int getAmount(EditText editText){
+        String s_amount = editText.getText().toString();
         int amount = 0;
         try{
-        amount = Integer.parseInt(s_amount); }
-        catch (NumberFormatException e) {}
+            amount = Integer.parseInt(s_amount); }
+        catch (NumberFormatException e) {
+            Log.e("InstantPaymentExample", "Error in receiving user input");
+        }
+        return amount;
+    }
+
+    public void makeDonation(View view){
+        //get the user input then convert it to an integer
+        int amount = getAmount((EditText) findViewById(R.id.user_donation_input));
 
         PayPalPayment payment =
                 new PayPalPayment(new BigDecimal(amount), "USD", "Test Payment", PayPalPayment.PAYMENT_INTENT_SALE);
@@ -73,7 +77,7 @@ public class PayPal extends AppCompatActivity {
         startActivityForResult(p_intent, REQUEST_CODE_PAYMENT);
     }
 
-    private void sendAuthorizationToServer(PayPalAuthorization authorization) {
+    private void sendAuthorizationToServer(PayPalAuthorization authorization) throws JSONException{
         /**
          * Send authorization response to server, where it can
          * exchange the authorization code for OAuth access and refresh tokens.
@@ -88,24 +92,24 @@ public class PayPal extends AppCompatActivity {
     }
 
     //used for future payments (pledges)
-    public void makePledgeaa(View view) {
+    public void onMakePledgePressed(View view) {
         // Get the Client Metadata ID from the SDK
         String metadataId = PayPalConfiguration.getClientMetadataId(this);
 
-        //  Log.i("FuturePaymentExample", "Client Metadata ID: " + metadataId);
+        //future_payment_amount (amount to be paid later)
+        Log.i("FuturePaymentExample", "Client Metadata ID: " + metadataId);
         // TODO: Send metadataId and transaction details to your server for processing with paypal
         // displayResultText("Client Metadata Id received from SDK");
     }
 
     public void makePledge(View pressed) {
-        Intent intent = new Intent(this, PayPalFuturePaymentActivity.class);
+        future_payment_amount = getAmount((EditText) findViewById(R.id.user_pledge_input));
 
-        PayPalPayment payment =
-                new PayPalPayment(new BigDecimal(100), "USD", "Test future payment", PayPalPayment.PAYMENT_INTENT_SALE);
+        Intent intent = new Intent(this, PayPalFuturePaymentActivity.class);
 
         // send the same configuration for restart resiliency
         intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, p_configuration);
-        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+
         startActivityForResult(intent, REQUEST_CODE_FUTURE_PAYMENT);
     }
 
@@ -113,28 +117,50 @@ public class PayPal extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == REQUEST_CODE_PAYMENT || requestCode == REQUEST_CODE_FUTURE_PAYMENT) {
+        if (requestCode == REQUEST_CODE_FUTURE_PAYMENT){
+            if (resultCode == Activity.RESULT_OK) {
+                PayPalAuthorization auth = data
+                        .getParcelableExtra(PayPalFuturePaymentActivity.EXTRA_RESULT_AUTHORIZATION);
+                if (auth != null) {
+                    try {
+                        //String authorization_code = auth.getAuthorizationCode();
+
+                        sendAuthorizationToServer(auth);
+                    }
+                    catch (JSONException e) {
+                        Log.e("FuturePaymentExample", "an extremely unlikely failure occurred: ", e);
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Log.i("FuturePaymentExample", "The user canceled.");
+            } else if (resultCode == PayPalFuturePaymentActivity.RESULT_EXTRAS_INVALID) {
+                Log.i("FuturePaymentExample",
+                        "Probably the attempt to previously start the PayPalService had an invalid PayPalConfiguration. Please see the docs.");
+            }
+        }
+
+        else if(requestCode == REQUEST_CODE_PAYMENT ) {
             if(resultCode == Activity.RESULT_OK) {
                 // confirm that payment worked
                 PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
                 if(confirmation != null) {
                     String status = confirmation.getProofOfPayment().getState();
-                    if(status.equals("approved")) // if the payment worked, the state equals approved
-                        p_response.setText("payment approved");
+                    if(status.equals("Approved")) // if the payment worked, the state equals approved
+                        Log.i("InstantPaymentExample", "Payment approved");
                     else
-                        p_response.setText("error in the payment");
+                        Log.i("InstantPaymentExample", "error in the payment");
                 }
                 else
-                    p_response.setText("confirmation is null");
+                    Log.i("InstantPaymentExample", "confirmation is null");
             }
         }
     }
 
-   /* @Override
+    @Override
     public void onDestroy() {
         // Stop service when done
         stopService(new Intent(this, PayPalService.class));
         super.onDestroy();
-    }*/
+    }
 
 }
---
+++ README.txt
@@ -0,0 +1,12 @@
+PayPal test implementation
+
+Can facilitate instant payments
+Implementation of future payments is in progress. 
+Contains skeleton code, but still need to implement the interaction with the server
+This includes sending authorization to the server, metadataId and transaction details
+
+Information regarding implementation of future payments:
+https://github.com/paypal/PayPal-Android-SDK/blob/master/docs/future_payments_mobile.md
+
+Information regarding server side of implementation:
+https://github.com/paypal/PayPal-Android-SDK/blob/master/docs/future_payments_server.md
