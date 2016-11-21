9ba290b333574d2d6a78dbeb1d9f51f7dacc3022
Selina Suen <s.suen@alumni.ubc.ca>
2016-11-20 22:03:00 -0800

    Future payments with server

--- FloatProject/app/src/main/java/com/cpen321/floatproject/FuturePaymentAgreement.java
+++ FloatProject/app/src/main/java/com/cpen321/floatproject/FuturePaymentAgreement.java
@@ -24,6 +24,15 @@ import com.paypal.api.payments.FuturePayment;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.PayPalRESTException;
 
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FuturePaymentAgreement extends AppCompatActivity {
 
     private DatabaseReference databaseref = FirebaseDatabase.getInstance().getReference();
@@ -33,8 +42,12 @@ public class FuturePaymentAgreement extends AppCompatActivity {
     String userid;
     //set environment as the testing sandbox (can also be set to do actual transactions)
     private static final String CONFIG_ENVIRONMENT = PayPalConfiguration.ENVIRONMENT_SANDBOX;
-    private static final String CLIENT_ID = "AaeuSB3UXO3lpq6BSvl6wan9CzqH71hDWnF2K8iBKgJPGGBtkEAvoTQ-ooxWWaCioyoYDE-x0wgHjsuQ";
-    private static final String CLIENT_SECRET = "ED5YbURCLIgiNGuO8OfL9yY3MPUNHvejc_0MjVCzKhxR_v1pQVn9MqO944bdEMUp1TGiF-V65VU9LMhW";
+    private static final String CLIENT_ID =
+            "AaeuSB3UXO3lpq6BSvl6wan9CzqH71hDWnF2K8iBKgJPGGBtkEAvoTQ-ooxWWaCioyoYDE-x0wgHjsuQ";
+    private static final String CLIENT_SECRET =
+            "ED5YbURCLIgiNGuO8OfL9yY3MPUNHvejc_0MjVCzKhxR_v1pQVn9MqO944bdEMUp1TGiF-V65VU9LMhW";
+    private static final String GET_REFRESH =
+            "http://ec2-54-213-91-175.us-west-2.compute.amazonaws.com/paypal-sdk/test_refresh.php";
 
 //    private static final String CLIENT_ID = "AX8PdEKmNADu_-4PQJ5sGojLRhWzV4yLXkOb9tGVRi_yLsii9a7SNNQ4g_VOr7VexhmjDgXUvf2vqZaN";
 //    private static final String CLIENT_SECRET = "EC8mkhijObvyvT29FZzGQXJDaU6F6mS-IxUcUWHLFIhruWvoUPEP8wzspEPJYae0w9jUEgK3yUZzAwi_";
@@ -81,17 +94,58 @@ public class FuturePaymentAgreement extends AppCompatActivity {
     }
 
     private void sendAuthorizationToServer(PayPalAuthorization authorization) throws PayPalRESTException {
-        String authorization_code = authorization.getAuthorizationCode();
-        String metadata_id = PayPalConfiguration.getClientMetadataId(this);
+        final String authorization_code = authorization.getAuthorizationCode();
+        final String metadata_id = PayPalConfiguration.getClientMetadataId(this);
         String environment = "sandbox";
 
-        Log.i("auth", authorization_code);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest putRequest = new StringRequest(Request.Method.POST, GET_REFRESH,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        // The server should return a refresh token
+                        Log.d("Refresh Token Response", response);
+                        String refresh_token = response;
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d("Error.Response", "Something Went Wrong");
+                    }
+                }
+        ) {
+            // We send the strings we want to the server in this method
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String> ();
+                //for future payments
+                params.put("authorizationCode", authorization_code);
+                params.put("clientMetadataId", metadata_id);
+                params.put("clientSecret",CLIENT_SECRET);
+                params.put("clientId",CLIENT_ID);
+                return params;
+            }
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/x-www-form-urlencoded");
+                return params;
+            }
 
-       // APIContext api_context = new APIContext(CLIENT_ID, CLIENT_SECRET, "sandbox");
-        APIContext api_context = new APIContext(CLIENT_ID, CLIENT_SECRET, "sandbox");
-        String refresh_token = FuturePayment.fetchRefreshToken(api_context, authorization_code);
-        Log.i("refresh", refresh_token);
-        api_context.setRefreshToken(refresh_token);
+        };
+        requestQueue.add(putRequest);
+
+       APIContext api_context = new APIContext(CLIENT_ID, CLIENT_SECRET, "sandbox");
+        //APIContext api_context = new APIContext(CLIENT_ID, CLIENT_SECRET, "sandbox");
+        //String refresh_token = FuturePayment.fetchRefreshToken(api_context, authorization_code);
+        //Log.i("refresh", refresh_token);
+        //api_context.setRefreshToken(refresh_token);
 
         Gson gson = new Gson();
         String json_api_context = gson.toJson(api_context);
