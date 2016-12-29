5c49bbccc42372704664b5c642350aa9ccc810e0
Samuel Farinas <samuelfarinas@gmail.com>
2016-12-28 22:37:46 -0800

    Added skeleton user_details.xml

---
+++ FloatProject/app/src/main/java/com/cpen321/floatproject/UserDetails.java
@@ -0,0 +1,15 @@
+package com.cpen321.floatproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sfarinas on 12/28/2016.
+ */
+public class UserDetails extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_details);
+    }
+}
---
+++ FloatProject/app/src/main/java/com/cpen321/floatproject/UserDetailsFragment.java
@@ -0,0 +1,36 @@
+package com.cpen321.floatproject;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+/**
+ * Created by sfarinas on 12/28/2016.
+ */
+public class UserDetailsFragment extends Fragment {
+    View view; //this fragment
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        //inflate the layout for this fragment
+        view = inflater.inflate(R.layout.user_details_fragment, container, false);
+
+        ((ImageButton)view.findViewById(R.id.user_details_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(view.getContext(), UserDetails.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+
+    }
+}
