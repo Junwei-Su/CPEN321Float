27ee49cf949d066c767a8a96cfecbfd184b27f0f
Richard Xie <xiezihao@yahoo.ca>
2016-12-30 17:14:27 -0800

    Update AlgorithmTest.java

--- FloatProject/app/src/test/java/com/cpen321/floatproject/AlgorithmTest.java
+++ FloatProject/app/src/test/java/com/cpen321/floatproject/AlgorithmTest.java
@@ -47,6 +47,20 @@ public class AlgorithmTest {
             Assert.assertTrue(false);
         }
     }
+    @Test
+    public void testCalculateDistance3(){
+        LatLng loc1 = new LatLng(10.92018392,20.2837128);
+        LatLng loc2 = new LatLng(30.0000,50.111333);
+        double expectedResult = 3742;
+        double errorMargin = 0.5;
+        double distance  = Algorithms.calculateDistance(loc1, loc2);
+        if(Math.abs(distance-expectedResult)/distance <= errorMargin){
+            Assert.assertTrue(true);
+        }
+        else{
+            Assert.assertTrue(false);
+        }
+    }
 
     @Test
     public void testStringToDate1() {
@@ -80,6 +94,27 @@ public class AlgorithmTest {
     }
 
     @Test
+    public void testStringToDate4() {
+        String str = "1000-10-01";
+        Date dtout = Algorithms.string_to_date(str);
+        Date dt = new Date(-900,9,1);
+        if(dtout.equals(dt))
+            assert true;
+        else
+            assert false;
+    }
+    @Test
+    public void testStringToDate5(){
+        String str = "--09-01";
+        Date dtout = Algorithms.string_to_date(str);
+        if(dtout == null)
+            assert true;
+        else
+            assert false;
+    }
+
+
+    @Test
     public void testDateToString1(){
         Date dt = new Date(99,9,1);
         String str = "1999-10-01";
@@ -98,4 +133,24 @@ public class AlgorithmTest {
         else
             assert false;
     }
+    @Test
+    public void testDateToString3(){
+        Date dt = new Date(94,0,1);
+        String str = "1994-01-01";
+        if(Algorithms.date_to_string(dt).equals(str))
+            assert true;
+        else
+            assert false;
+    }
+    @Test
+    public void testDateToString4(){
+        Date dt = new Date(94,1,-2);
+        String str = "1994-01-29";
+        System.out.println(dt);
+        if(Algorithms.date_to_string(dt).equals(str))
+            assert true;
+        else
+            assert false;
+    }
+
 }
