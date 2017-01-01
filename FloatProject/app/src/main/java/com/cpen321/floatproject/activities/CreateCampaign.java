70ae28c2892eb413604982f2a4669c6fde48f8c1
Little_town <junweisu.working@gmail.com>
2016-12-31 18:02:46 -0800

    minor change on time _length

--- FloatProject/app/src/main/java/com/cpen321/floatproject/activities/CreateCampaign.java
+++ FloatProject/app/src/main/java/com/cpen321/floatproject/activities/CreateCampaign.java
@@ -59,7 +59,7 @@ public class CreateCampaign extends AppCompatActivity {
 
     //clarence added for destination field
     private String destination;
-    long time_length = 10;
+    long time_length;
     private String campPic_url = "default_camp_pic.jpg"; //default picture
     private CampsDBInteractor campsDBInteractor = new CampsDBInteractor();
 
